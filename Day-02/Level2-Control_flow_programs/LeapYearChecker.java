c67010c37ab6b5794560839e75ee3a4d1c9de055
Shristy1503 <shriimishra15@gmail.com>
2025-01-12 22:27:33 +0530

    leap year checker push it

---
+++ Day-02/Level2-Control_flow_programs/LeapYearChecker.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+   public class LeapYearChecker{
+      public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for input
+        System.out.print("Enter a year : ");
+        int year = scanner.nextInt();
+
+        // Check if the year is valid
+        if (year < 1582) {
+            System.out.println("Year must be 1582 or later.");
+        } else {
+            // Part 1: Using multiple if-else statements
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        System.out.println(year + " is a Leap Year.");
+                    } else {
+                        System.out.println(year + " is not a Leap Year.");
+                    }
+                } else {
+                    System.out.println(year + " is a Leap Year.");
+                }
+                } else {
+                System.out.println(year + " is not a Leap Year.");
+            }  
+        }
+      }
+  }
