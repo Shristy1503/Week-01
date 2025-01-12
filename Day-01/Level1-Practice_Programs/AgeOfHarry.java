2d32156111dbdb9c77614c3f3de5602d555b097a
Shristy1503 <shriimishra15@gmail.com>
2025-01-12 20:38:01 +0530

    age of harry program

---
+++ Day-01/Level1-Practice_Programs/AgeOfHarry.java
@@ -0,0 +1,12 @@
+ import java.util.*;
+   public class AgeOfHarry{
+     public static void main(String[]args){
+	   int birthYear = 2000;
+	   int currentYear = 2024;
+	   
+	   //calculate present age of harry
+	   
+	   int presentAge = currentYear - birthYear;
+	   System.out.println("Harry's age in 2024 is: " + presentAge);
+	 }
+   }
