4cfd05a30f736bc03a6917504456e70d03b321b1
Shristy1503 <shriimishra15@gmail.com>
2025-01-12 22:39:45 +0530

    day of week push it

---
+++ Day-02/Level3-Control_flow_programs/DayOfWeek.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+  public class DayOfWeek{
+    public static void main(String[] args) {
+        
+        // Parse in the command-line to take arguments
+        int m = Integer.parseInt(args[0]); // Month
+        int d = Integer.parseInt(args[1]); // Day
+        int y = Integer.parseInt(args[2]); // Year
+
+        // Apply the formulas
+        int y0 = y - (14 - m) / 12;
+        int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+        int m0 = m + 12 * ((14 - m) / 12) - 2;
+        int d0 = (d + x + (31 * m0) / 12) % 7;
+        System.out.println(d0);
+    }
+}
