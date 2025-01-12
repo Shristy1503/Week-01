23b0440c301cdf380a91e0b264e104c57be4a7fd
Shristy1503 <shriimishra15@gmail.com>
2025-01-12 21:51:11 +0530

    sum of number till zero or negative input push it

---
+++ Day-02/Level1-Control_flow_programs/SumTillZeroOrNegative.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+    public class SumTillZeroOrNegative{
+	  public static void main(String[]args){
+	  
+	    // initialize variable of double
+		double total = 0.0;
+		while(true){
+		// taking user inout till input value is 0
+		
+	       Scanner input = new Scanner(System.in);
+		   System.out.println("Enter a number ");
+		   double number = input.nextDouble();
+		    
+			if(number == 0 || number < 0){
+			  break; 
+			}
+			total += number;
+		}
+		  System.out.println("Sum of number is " + total);
+		  
+	  }
+	}
+		
